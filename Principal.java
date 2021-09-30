592282fc8c3af35854634f3a725a8b559cba3a5e
xKaue007 <kaueoliveira92@gmail.com>
2021-09-30 19:21:17 +0000

    2.0

--- Principal.java
+++ Principal.java
@@ -4,7 +4,13 @@ public class Principal{
 
     Funcionario f=new Funcionario();
     f.setNome("Kauê");
+    f.setCpf("123.456.789-10");
+    f.setDepartamento("T.I");
+    f.setSalario(5600);
 
-    System.out.println(f.getNome());
+    System.out.println("O nome do funcionário é "+f.getNome());
+    System.out.println("CPF do Funcionário :"+f.getCpf());
+    System.out.println("Pertence ao deartamento de "+f.getDepartamento());
+    System.out.println("Recebendo um salário no valor de "+f.getSalario());
     }
 }
